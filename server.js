6af22faa4e671305914ec91a40fd8d57359b5d0a
Pavan Chinnadorai <pavanchinnadoraij1@gmail.com>
2025-06-06 13:04:24 +0530

    Fix server.js for Vercel deployment: add root route, remove app.listen, export app

--- server.js
+++ server.js
@@ -1,14 +1,12 @@
-
-const express=require("express");
-const mongoose=require("mongoose")
+const express = require("express");
+const mongoose = require("mongoose");
 const cors = require('cors');
 const dotenv = require('dotenv');
 dotenv.config();
 
-
-const app=express();
-const authRoutes=require("./Routes/authRoutes");
-const blogRoutes=require("./Routes/blogRoutes")
+const app = express();
+const authRoutes = require("./Routes/authRoutes");
+const blogRoutes = require("./Routes/blogRoutes");
 
 app.use(cors({
   origin: 'http://localhost:3000',
@@ -20,15 +18,20 @@ app.use(express.json());
 app.use('/api/auth', authRoutes);
 app.use('/api/blogs', blogRoutes);
 
+// Add a simple root route
+app.get('/', (req, res) => {
+  res.send('API is running');
+});
 
-
-
-
+// Connect to MongoDB (optional: you might want to move this outside the handler if issues arise)
 mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
-.then(() => console.log('MongoDB connected'))
-.catch(err => console.error(err));
+  .then(() => console.log('MongoDB connected'))
+  .catch(err => console.error(err));
 
-const PORT = process.env.PORT;
-app.listen(PORT,()=>{
-    console.log(`Server running on port ${PORT}`)
-})
+// Remove this for serverless deployment
+// const PORT = process.env.PORT;
+// app.listen(PORT, () => {
+//   console.log(`Server running on port ${PORT}`);
+// });
+
+module.exports = app;  // Export app for Vercel
