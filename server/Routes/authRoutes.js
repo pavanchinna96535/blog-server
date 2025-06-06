51bec8311de5b9c8726b6b65acef85f44b760092
Pavan Chinnadorai <pavanchinnadoraij1@gmail.com>
2025-06-06 09:43:23 +0530

    Stop tracking backend folder

--- server/Routes/authRoutes.js
+++
@@ -1,9 +0,0 @@
-
-const express = require('express');
-const router = express.Router();
-const { register, login } = require('../Controllers/authControllers');
-
-router.post('/register', register);
-router.post('/login', login);
-
-module.exports = router;
